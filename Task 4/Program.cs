372537c72c5bf8e39b0324f71715e572d91f4b1a
Andrey Mospanov <mospanov-andrey@ya.ru>
2020-02-23 13:24:49 +0400

    добавил задание 4 с введением логина и пароля

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -11,7 +11,71 @@ namespace Task_4
         //написать программу: пользователь вводит логин и пароль, программа пропускает его дальше или
         //не пропускает.С помощью цикла do while ограничить ввод пароля тремя попытками.
     {
-      
-      
+        static bool Log(string login)
+        {
+            if (login == "root")
+            { return true; }
+            else { return false; }
+        }
+        static bool Pass(string password)
+        {
+            if (password == "GEEKBRAINS")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region логин пароль без ограничения попыток
+            //int count = 0;//Количество попыток
+            //string login;
+            //string password;
+            //do
+            //{
+            //    WriteLine("Введите Логин");
+            //    login = ReadLine();
+            //    WriteLine("Введите Пароль");
+            //    password = ReadLine();
+            //    count++;
+            //}
+            //while (  password != "GEEKBRAINS" || login != "root");
+            //WriteLine($"Добро пожаловать в систему, У вас было {count} попыток");
+            //Pause();
+            #endregion
+
+            int count=0;
+            string login;
+            string password;
+            do
+            {
+                WriteLine("Введите Логин");
+                login = ReadLine();
+                Log(login);
+                WriteLine("Введите Пароль");
+                password = ReadLine();
+                Pass(password);
+                count++;
+                if (Log(login) == true && Pass(password) == true)
+                {
+                    WriteLine("Добро пожаловать в систему");
+                    break;
+                }
+            }
+            while (count < 3);
+            if (count == 3)
+            { 
+                WriteLine("Превышено количество попыток"); 
+            }
+            Pause();
+
+        }
+
+
+
+
     }
 }
